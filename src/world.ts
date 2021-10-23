648b4d18acd9ad8f71262bb896189ab1c572202f
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-23 17:48:54 +0900

    ESLintとPrettierの導入-修正2

--- src/world.ts
+++ src/world.ts
@@ -5,15 +5,9 @@ export default class World {
     this.message = message
   }
 
-  public sayHello　(elem: HTMLElement | null) {
+  public sayHello(elem: HTMLElement | null) {
     if (elem) {
       elem.innerText = this.message
     }
   }
-
-
-
-
-
-
 }
